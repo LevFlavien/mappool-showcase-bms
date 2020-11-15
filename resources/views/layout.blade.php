f8380b58a36fbae2cc371b1f23c21396b83174bd
LevFlavien <levesque.flavien@outlook.fr>
2020-11-15 18:05:08 +0100

    feat(layout): remove shitty text

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,8 +4,6 @@
     <link href="style.css" rel="stylesheet">
 </head>
 
-<p>poulet's impressive ghetto system to help todestrieb showcase the mappool of the best tournament ever but ruining the reputation of osufrlive at the same time based on glorious php 7.x</p>
-
 <body>@yield('content')</body>
 
 </html>
